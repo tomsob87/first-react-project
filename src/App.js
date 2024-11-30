a7145c5e001903c9571979ce936641e396e7ae2e
Tomasz Sobczyk <tomasz.pawel.sobczyk@gmail.com>
2024-11-30 19:40:40 +0100

    Add NotFound

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import Home from './components/Home/Home';
 import NavBar from './components/NavBar/NavBar';
 import Container from './components/Container/Container';
+import About from './components/About/About';
+import Favorite from './components/Favorite/Favorite';
+import NotFound from './components/NotFound/NotFound';
+import { Routes, Route } from 'react-router-dom';
+
 
 const App = () => {
   return (
     <>
       <NavBar />
       <Container>
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/favorite" element={<Favorite />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </Container>
     </>  
   );
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import '../../styles/settings.scss';
+import PageTitle from '../PageTitle/PageTitle';
+
+const NotFound = () => {
+    return (
+        <div>
+            <PageTitle>404</PageTitle>
+            <p>Page not found.</p>
+        </div>
+    );
+};
+
+export default NotFound;
